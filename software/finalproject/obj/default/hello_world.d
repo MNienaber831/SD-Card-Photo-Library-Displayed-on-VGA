7917e28ba3206bdc6851691aee407fe483030b0e
MNienaber831 <micaelanienaber@yahoo.com>
2021-12-11 16:06:21 -0800

    Create hello_world.d

---
+++ software/finalproject/obj/default/hello_world.d
@@ -0,0 +1,33 @@
+obj/default/hello_world.o: hello_world.c \
+ ../finalproject_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../finalproject_bsp//HAL/inc/io.h \
+ ../finalproject_bsp//HAL/inc/alt_types.h \
+ ../finalproject_bsp//drivers/inc/Altera_UP_SD_Card_Avalon_Interface.h \
+ ../finalproject_bsp//HAL/inc/alt_types.h \
+ ../finalproject_bsp//HAL/inc/sys/alt_dev.h ../finalproject_bsp/system.h \
+ ../finalproject_bsp/linker.h \
+ ../finalproject_bsp//HAL/inc/sys/alt_llist.h \
+ ../finalproject_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../finalproject_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h
+
+../finalproject_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../finalproject_bsp//HAL/inc/io.h:
+
+../finalproject_bsp//HAL/inc/alt_types.h:
+
+../finalproject_bsp//drivers/inc/Altera_UP_SD_Card_Avalon_Interface.h:
+
+../finalproject_bsp//HAL/inc/alt_types.h:
+
+../finalproject_bsp//HAL/inc/sys/alt_dev.h:
+
+../finalproject_bsp/system.h:
+
+../finalproject_bsp/linker.h:
+
+../finalproject_bsp//HAL/inc/sys/alt_llist.h:
+
+../finalproject_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../finalproject_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
